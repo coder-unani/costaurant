bed0323932b3d4151af523ce05313d33f204dd84
UNANI <coder.unani@gmail.com>
2024-03-28 23:48:42 +0900

    [feat] 메뉴 상세페이지 구현중

--- menus/views.py
+++ menus/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 def index(request):
-    return render(request, "menus/index.html")
+    today = datetime.today().date()
+    context = { "date": today }
+    return render(request, "menus/index.html", context=context)
+
+def detail(request, food):
+    context = { "food": food }
+    return render(request, "menus/detail.html", context=context)
